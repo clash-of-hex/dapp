a48fe8a78e832007d037e69efd992ccf3ff381d3
Ilyar <761285+ilyar@users.noreply.github.com>
2022-11-15 14:05:54 +0100

    v1: replace into parcel

--- src/app-v1/src/index.js
+++ src/app-v1/src/index.js
@@ -1,17 +1,9 @@
 "use strict"
-//Import CSS
-import './styles/main.scss'
 //Import JS
 import * as DISPLAY from './scripts/display.js'
 import * as CONTROLS from './scripts/controls.js'
 import * as PROVIDER from './scripts/ever.js'
 
 //https://github.com/flauwekeul/honeycomb
-//console.clear();
-
 DISPLAY.initiateMap(PROVIDER)
 CONTROLS.initiateControls()
-
-if (module.hot) {
-    module.hot.accept()
-}
